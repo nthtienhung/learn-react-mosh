cad1cdc89659f82d951cb1444412efc6d7a62a5a
k3 <nthtienhung123@gmail.com>
2024-10-16 09:21:02 +0700

    test

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,7 +1,16 @@
-import ListGroup from "./components/ListGroup";
+// import ListGroup from "./components/ListGroup";
 
-function App() {
-  return <div><ListGroup /></div>;
+// function App() {
+//   return <div><ListGroup /></div>;
+// }
+
+// export default App;
+
+import Button from "./components/Button";
+function App () {
+  return (
+  <div><Button /></div>    
+  );
 }
 
 export default App;
---
+++ vite-project/src/components/Alert.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ListGroup from './ListGroup'
+const Alert = () => {
+  return (
+    <div>Alert</div>
+  )
+}
+
+export default Alert
